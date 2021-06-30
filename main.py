f4c50306a4b4b890acdfb319913337f08995c2be
MobileApps <mhlangat@gmail.com>
2021-06-30 13:08:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+print('Flour')
+
+counter = 0 # Used the control the loop
+while(counter < 3):
+  print('1 Egg')
+  counter += 1
+
+print('Milk')
+
+counter = 0
+while(counter < 2):
+  print('1 cup Sugar')
+  counter+=1
+
+print('Baking Soda')
+
+counter = 0
+while(counter < 60):
+  print('1 min Bake')
+  counter+=1
+
+counter = 0
+while(counter < 4):
+  print('Eat a slice of Cozonac')
+  counter += 1
+
+
